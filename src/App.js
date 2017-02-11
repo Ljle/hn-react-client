2105950a7d37bedcad4cb061e05c45c33da6388a
Luis León <ljle.06@gmail.com>
2017-02-10 22:52:29 -0400

    Some refactoring

--- src/App.js
+++ src/App.js
@@ -1,121 +1,91 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-const DEFAULT_QUERY = 'redux';
-const DEFAULT_PAGE = 0;
-const DEFAULT_HPP = '100';
+const largeColumn = {width: '40%'}
+const midColumn = {width: '30%'}
+const smallColumn = {width: '10%'}
 
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-const PARAM_HPP = 'hitsPerPage=';
+const DEFAULT_QUERY = 'redux'
+const DEFAULT_PAGE = 0
+const DEFAULT_HPP = '20'
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1'
+const PATH_SEARCH = '/search'
+const PARAM_SEARCH = 'query='
+const PARAM_PAGE = 'page='
+const PARAM_HPP = 'hitsPerPage='
 
 class App extends Component {
-
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.state = {
-      results: null,
-      searchKey: '',
-      searchTerm: DEFAULT_QUERY,
-    };
+    this.state = {results: null, searchKey: '', searchTerm: DEFAULT_QUERY}
 
-    this.needsToSearchTopstories = this.needsToSearchTopstories.bind(this);
-    this.setSearchTopstories = this.setSearchTopstories.bind(this);
-    this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this);
-    this.onSearchChange = this.onSearchChange.bind(this);
-    this.onSearchSubmit = this.onSearchSubmit.bind(this);
-    this.onDismiss = this.onDismiss.bind(this);
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this)
+    this.setSearchTopStories = this.setSearchTopStories.bind(this)
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
+    this.onSearchChange = this.onSearchChange.bind(this)
+    this.onSearchSubmit = this.onSearchSubmit.bind(this)
+    this.onSearchChange = this.onSearchChange.bind(this)
+    this.onDismiss = this.onDismiss.bind(this)
+  }
+
+  needsToSearchTopStories(searchTerm){
+    return !this.state.results[searchTerm]
+  }
+
+  setSearchTopStories(result) {
+    const {hits, page} = result
+    const {searchKey, results} = this.state
+    const oldHits = results && results[searchKey]
+      ? results[searchKey].hits
+      : []
+    const updatedHits = [...oldHits, ...hits]
+    this.setState({
+      results: {...results, [searchKey]: {hits: updatedHits, page}}
+    })
+  }
+
+  fetchSearchTopStories(searchTerm, page) {
+    fetch(
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
+    )
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
   }
 
   componentDidMount() {
-    const { searchTerm } = this.state;
-    this.setState({ searchKey: searchTerm });
-    this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
-  }
-
-  setSearchTopstories(result) {
-    const { hits, page } = result;
-    const { searchKey, results } = this.state;
-
-    const oldHits = results && results[searchKey]
-      ? results[searchKey].hits
-      : [];
-
-    const updatedHits = [
-      ...oldHits,
-      ...hits
-    ];
-
-    this.setState({
-      results: {
-        ...results,
-        [searchKey]: { hits: updatedHits, page }
-      }
-    });
-  }
-
-  fetchSearchTopstories(searchTerm, page) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopstories(result));
-  }
-
-  needsToSearchTopstories(searchTerm) {
-    return !this.state.results[searchTerm];
-  }
-
-  onSearchChange(event) {
-    this.setState({ searchTerm: event.target.value });
-  }
-
-  onSearchSubmit(event) {
-    const { searchTerm } = this.state;
-    this.setState({ searchKey: searchTerm });
-
-    if (this.needsToSearchTopstories(searchTerm)) {
-      this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
-    }
-
-    event.preventDefault();
+    const {searchTerm} = this.state
+    this.setState({searchKey: searchTerm})
+    this.fetchSearchTopStories(searchTerm, DEFAULT_PAGE)
   }
 
   onDismiss(id) {
-    const { searchKey, results } = this.state;
-    const { hits, page } = results[searchKey];
-
-    const isNotId = item => item.objectID !== id;
-    const updatedHits = hits.filter(isNotId);
-
+    const {searchKey, results} = this.state
+    const {hits, page} = results[searchKey]
+    const updatedHits = hits.filter(c => c.objectID !== id)
     this.setState({
-      results: {
-        ...results,
-        [searchKey]: { hits: updatedHits, page }
-      }
-    });
+      results: {...results, [searchKey]: {hits: updatedHits, page}}
+    })
+  }
+
+  onSearchChange(event) {
+    this.setState({searchTerm: event.target.value})
+  }
+
+  onSearchSubmit(event) {
+    const {searchTerm} = this.state
+    this.setState({searchKey: searchTerm})
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm, DEFAULT_PAGE)
+    }
+    event.preventDefault()
   }
 
   render() {
-    const {
-      searchTerm,
-      results,
-      searchKey
-    } = this.state;
-
-    const page = (
-      results &&
-      results[searchKey] &&
-      results[searchKey].page
-    ) || 0;
-
-    const list = (
-      results &&
-      results[searchKey] &&
-      results[searchKey].hits
-    ) || [];
-
+    const {searchTerm, results, searchKey} = this.state
+    const page = (results && results[searchKey] && results[searchKey].page) || 0
+    const list = (results && results[searchKey] && results[searchKey].hits) || []
     return (
       <div className="page">
         <div className="interactions">
@@ -127,54 +97,43 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table
-          list={list}
-          onDismiss={this.onDismiss}
-        />
+        <Table list={list} onDismiss={this.onDismiss} />
         <div className="interactions">
-          <Button onClick={() => this.fetchSearchTopstories(searchKey, page + 1)}>
+          <Button
+            onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
+          >
             More
           </Button>
         </div>
       </div>
-    );
+    )
   }
 }
 
-const Search = ({
-  value,
-  onChange,
-  onSubmit,
-  children
-}) =>
+const Search = ({children, onChange, value, onSubmit}) => (
   <form onSubmit={onSubmit}>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-    <button type="submit">
-      {children}
-    </button>
+    <input type="text" onChange={onChange} value={value} />
+    <button type="submit">{children}</button>
   </form>
+)
 
-const Table = ({ list, onDismiss }) =>
+const Table = ({list, onDismiss}) => (
   <div className="table">
-    { list.map(item =>
+    {list.map(item => (
       <div key={item.objectID} className="table-row">
-        <span style={{ width: '40%' }}>
+        <span style={largeColumn}>
           <a href={item.url}>{item.title}</a>
         </span>
-        <span style={{ width: '30%' }}>
+        <span style={midColumn}>
           {item.author}
         </span>
-        <span style={{ width: '10%' }}>
+        <span style={smallColumn}>
           {item.num_comments}
         </span>
-        <span style={{ width: '10%' }}>
+        <span style={smallColumn}>
           {item.points}
         </span>
-        <span style={{ width: '10%' }}>
+        <span style={smallColumn}>
           <Button
             onClick={() => onDismiss(item.objectID)}
             className="button-inline"
@@ -183,16 +142,15 @@ const Table = ({ list, onDismiss }) =>
           </Button>
         </span>
       </div>
-    )}
+    ))}
   </div>
+)
 
-const Button = ({ onClick, className = '', children }) =>
-  <button
-    onClick={onClick}
-    className={className}
-    type="button"
-  >
+const Button = ({onClick, className = '', children}) => (
+  <button onClick={onClick} className={className} type="button">
     {children}
   </button>
+)
 
-export default App;
+export default App
+
